174e1a9e00ce7fb52d93e30c8acff9b91c35692c
santiagogoncalvez <santiago.goncalvez.l@gmail.com>
2026-01-25 21:20:09 -0300

    refactor(portfolio): optimizar narrativa del portfolio y descripciones de proyectos con foco en producto. Cambiar paths largos por alias de vite. Modificar estilos del index.astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,25 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
+import path from "node:path";
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  },
-
+   vite: {
+      plugins: [tailwindcss()],
+      resolve: {
+         alias: {
+            "@": path.resolve("./src"),
+            "@components": path.resolve("./src/components"),
+            "@sections": path.resolve("./src/components/sections"),
+            "@shared": path.resolve("./src/components/shared"),
+            "@icons": path.resolve("./src/icons"),
+            "@assets": path.resolve("./src/assets"),
+            "@styles": path.resolve("./src/styles"),
+            "@scripts": path.resolve("./src/scripts"),
+         },
+      },
+   },
 });
