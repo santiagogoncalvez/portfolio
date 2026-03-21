e4ed0b98383f650c60df9158ae5a622259b22a92
santiagogoncalvez <santiago.goncalvez.l@gmail.com>
2026-03-20 21:55:03 -0300

    refactor: crear constante navItems que es importada por el header y footer para construir sus navegaciones y que sean las mismas.

---
+++ src/constants/navItems.mjs
@@ -0,0 +1,10 @@
+export const navItems = [
+   { title: "Soluciones", label: "soluciones", url: "/#soluciones" },
+   { title: "Mi Proceso", label: "mi-proceso", url: "/#mi-proceso" },
+
+   { title: "Proyectos", label: "proyectos", url: "/#proyectos" },
+   { title: "Testimonios", label: "testimonios", url: "/#testimonios" },
+
+   { title: "Sobre mí", label: "sobre-mi", url: "/#sobre-mi" },
+   { title: "Contacto", label: "contacto", url: "/#contacto" },
+];
