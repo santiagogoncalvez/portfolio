17f61121c3357c45faa3ecd59c514ea8771fabf7
santiagogoncalvez <santiago.goncalvez.l@gmail.com>
2026-06-10 14:49:59 -0300

    refactor: simplificar estructura del portfolio y corregir animaciones Motion. Centrar los títulos de sección. Eliminar espacio del footer con respecto al contenido principal. Corregir todos los errores de astro y de tipos que había en el proyecto.

--- src/constants/navItems.mjs
+++ src/constants/navItems.mjs
@@ -1,10 +1,10 @@
 export const navItems = [
-   { title: "Soluciones", label: "soluciones", url: "/#soluciones" },
+   // { title: "Soluciones", label: "soluciones", url: "/#soluciones" },
    { title: "Mi proceso", label: "mi-proceso", url: "/#mi-proceso" },
 
    { title: "Proyectos", label: "proyectos", url: "/#proyectos" },
    { title: "Testimonios", label: "testimonios", url: "/#testimonios" },
 
-   { title: "Sobre mí", label: "sobre-mi", url: "/#sobre-mi" },
+   // { title: "Sobre mí", label: "sobre-mi", url: "/#sobre-mi" },
    { title: "Contacto", label: "contacto", url: "/#contacto" },
 ];
